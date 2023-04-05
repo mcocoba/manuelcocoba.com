6609551956e491684965acc16e0e96a39ef59a38
Manny Cocoba <manuelcocoba@gmail.com>
2023-04-05 10:10:26 -0700

    fix(map): update map-gl implementation

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,4 +1,4 @@
-import { useState, useRef } from "react";
+import { useRef } from "react";
 import ReactMapGL, { Marker } from "react-map-gl";
 
 const ICON = `M20.2,15.7L20.2,15.7c1.1-1.6,1.8-3.6,1.8-5.7c0-5.6-4.5-10-10-10S2,4.5,2,10c0,2,0.6,3.9,1.6,5.4c0,0.1,0.1,0.2,0.2,0.3
@@ -9,23 +9,19 @@ const SIZE = 20;
 
 export default function Map({ locations }) {
 
-  const [viewport, setViewport] = useState({
-    latitude: 35.3816221,
-    longitude: -52.790592,
-    zoom: 1.6,
-    bearing: 0,
-    pitch: 10
-  });
 
   const mapRef = useRef(null);
 
   return <ReactMapGL
-    {...viewport}
-    width="100%"
-    height="100%"
+    initialViewState={{
+      latitude: 35.3816221,
+      longitude: -52.790592,
+      zoom: 1.6,
+      bearing: 0,
+      pitch: 10
+    }}
+    style={{ width: '100%', height: '100%' }}
     mapStyle="mapbox://styles/mapbox/dark-v9"
-    onViewportChange={setViewport}
-    mapboxApiAccessToken={process.env.NEXT_PUBLIC_MAPBOX_KEY}
     ref={mapRef}
   >
     {locations.map((item, index) => (
@@ -33,8 +29,7 @@ export default function Map({ locations }) {
         <Marker
           key={`marker-${index}`}
           longitude={item.longitude}
-          latitude={item.latitude}
-          offsetLeft={5} >
+          latitude={item.latitude} >
           <svg
             height={SIZE}
             viewBox="0 0 24 24"
