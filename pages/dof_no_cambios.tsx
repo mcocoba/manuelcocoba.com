d7ed3c992a8118e6966bad64343007c110cce922
Manny Cocoba <manuelcocoba@gmail.com>
2026-06-12 11:39:04 -0700

    feat(dof): adding dof's for testing

---
+++ pages/dof_no_cambios.tsx
@@ -0,0 +1,13 @@
+import Container from '../components/Container';
+
+export default function DofNoCambios() {
+  return (
+    <Container title="DOF – Manuel Cocoba">
+      <div className="flex flex-col justify-center items-center min-h-screen">
+        <h1 className="text-center text-3xl md:text-5xl font-bold text-black dark:text-white">
+          NO CAMBIOS
+        </h1>
+      </div>
+    </Container>
+  );
+}
---
+++ pages/dof_si_cambios.tsx
@@ -0,0 +1,13 @@
+import Container from '../components/Container';
+
+export default function DofSiCambios() {
+  return (
+    <Container title="DOF – Manuel Cocoba">
+      <div className="flex flex-col justify-center items-center min-h-screen">
+        <h1 className="text-center text-3xl md:text-5xl font-bold text-black dark:text-white">
+          Cambios, nuevo impuesto de IVA fronterizo donde el IVA será del 20%
+        </h1>
+      </div>
+    </Container>
+  );
+}
